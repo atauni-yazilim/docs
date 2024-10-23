2048d755026aa35cb3544386818fe6559545f978
zumrudu-anka <osmandurdag@hotmail.com>
2024-10-23 19:22:18 +0300

    .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import { yandal } from "./Yandal";
 import { ciftAnaDal } from "./CiftAnadal";
 import { onLisansVeLisansEgitimOgretimVeSinavYonetmeligi } from "./onLisansVeLisansEgitimOgretimVeSinavYonetmeligi";
-import { danismanlik } from "./Danismanlik"; 
+import { danismanlik } from "./Danismanlik";
+import { yazOkulu } from "./YazOkulu";
 
 export const pages = [
     ciftAnaDal,
     yandal,
     onLisansVeLisansEgitimOgretimVeSinavYonetmeligi,
-    danismanlik
+    danismanlik,
+    yazOkulu
 ];
